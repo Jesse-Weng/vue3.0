4f83e18b5901fa4a94a020d568449b3fe8c2f7ba
jesse <jxweng@gizwits.com>
2020-12-24 23:15:19 +0800

    'feat：安装vue-router、配置vue-router、安装vuex'

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import router from './router/index'
+import vuex from './store/index'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(vuex)
+app.mount('#app')
