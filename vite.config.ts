13ee3eef3d2bde622df48a15eff2ad99e8d9da13
jesse <jxweng@gizwits.com>
2020-12-24 23:26:34 +0800

    'feat：配置vite'

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+
+import type { UserConfig } from 'vite';
+
+const viteConfig: UserConfig = {
+    /* 端口号 */
+    port: 3555,
+
+    /* hostname */
+    hostname: 'localhost',
+
+    /* 运行自动打开浏览器 */
+    open: false,
+};
+
+export default viteConfig;
