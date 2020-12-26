2737b6841c3e9677705f25bd89cb8e68165abb41
jesse <jxweng@gizwits.com>
2020-12-27 01:21:36 +0800

    'feat：vite配置文件别名'

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 
+
 import type { UserConfig } from 'vite';
+const path = require('path')
 
 const viteConfig: UserConfig = {
     /* 端口号 */
@@ -11,15 +13,23 @@ const viteConfig: UserConfig = {
     /* 运行自动打开浏览器 */
     open: false,
 
+    /* 配置别名 */
+    alias: {
+        '/@/': path.resolve(__dirname, './src'),
+        '/@views/': path.resolve(__dirname, './src/views'),
+        '/@components/': path.resolve(__dirname, './src/components'),
+        '/@utils/': path.resolve(__dirname, './src/utils'),
+    },
+
     /* proxy */
     proxy: {
         '/api': {
             target: 'https://uni937d4b0-6cc760.service.tcloudbase.com',
             changeOrigin: true,
+            ws: true,
             rewrite: path => path.replace(/^\/api/, '')
         }
     }
-
 };
 
 export default viteConfig;
