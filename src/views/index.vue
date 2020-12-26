4bf4d00f1aa7a54d91625ba9fcc629541cb307a1
jesse <jxweng@gizwits.com>
2020-12-27 01:27:41 +0800

    'feat：开发index.vue组件'

---
+++ src/views/index.vue
@@ -0,0 +1,55 @@
+<!--  -->
+<template>
+  <div class="home">
+    <zhihuHeader :day="day" :month="month" />
+    <zhihu-banner :bannerData="bannerData" />
+  </div>
+</template>
+
+<script lang="ts">
+import zhihuHeader from "../components/Header.vue";
+import zhihuBanner from "/@/components/Banner.vue";
+import { reactive, toRefs, computed, onBeforeMount } from "vue";
+import { getBannerList } from "../api/index";
+import utils from "../utils";
+
+export default {
+  components: {
+    zhihuHeader,
+    zhihuBanner,
+  },
+
+  setup() {
+    const state = reactive({
+      date: new Date(),
+      bannerData: [],
+    });
+
+    let day = computed(() => utils.formatTime(state.date)[2]);
+    let month = computed(() => utils.formatTime(state.date)[1]);
+
+    const getBannerData = async () => {
+      const res = await getBannerList();
+      state.bannerData = res.data.data;
+      console.log(state.bannerData);
+    };
+
+    onBeforeMount(() => {
+      getBannerData();
+    });
+
+    return {
+      ...toRefs(state),
+      day,
+      month,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  widows: 100%;
+  height: 100%;
+}
+</style>
